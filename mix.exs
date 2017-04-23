f6dd028ca144a9016697b6080d43c887c44e0cd9
Chris Alley <chris@chrisalley.info>
2017-04-23 13:04:04 +1000

    Add comeonin app and dependencies to mixfile

--- mix.exs
+++ mix.exs
@@ -17,9 +17,20 @@ defmodule Quill.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Quill, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+    [
+      mod: {Quill, []},
+      applications: [
+        :phoenix,
+        :phoenix_pubsub,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :phoenix_ecto,
+        :postgrex,
+        :comeonin
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +41,17 @@ defmodule Quill.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 3.0.2"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
