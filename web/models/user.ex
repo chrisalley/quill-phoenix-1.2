d265d8af0f30294eac3f4a735af867232eb6f4fc
Chris Alley <chris@chrisalley.info>
2017-04-23 15:05:02 +1000

    Change user registration process to encrypt password

--- web/models/user.ex
+++ web/models/user.ex
@@ -3,6 +3,7 @@ defmodule Quill.User do
 
   schema "users" do
     field :email, :string
+    field :password, :string, virtual: true
     field :encrypted_password, :string
     belongs_to :profile, Quill.Profile
 
@@ -14,7 +15,27 @@ defmodule Quill.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :encrypted_password])
-    |> validate_required([:email, :encrypted_password])
+    |> cast(params, [:email])
+    |> validate_required([:email])
+    |> validate_length(:email, min: 1, max: 254)
+  end
+
+  def registration_changeset(struct, params) do
+    struct
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_encrypted_password()
+  end
+
+  defp put_encrypted_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(
+          changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(pass)
+        )
+      _ ->
+        changeset
+    end
   end
 end
