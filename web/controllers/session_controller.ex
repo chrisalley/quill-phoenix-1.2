a488febc987e26a62260eb9afdc83aa64f09440d
Chris Alley <chris@chrisalley.info>
2017-04-23 18:19:09 +1000

    Implement log out functionality

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -22,6 +22,12 @@ defmodule Quill.SessionController do
     end
   end
 
+  def delete(conn, _) do
+    conn
+    |> logout()
+    |> redirect(to: wiki_path(conn, :index))
+  end
+
   defp login_with_email_and_password(conn, email, password) do
     user = Repo.get_by(Quill.User, email: email)
 
@@ -41,5 +47,9 @@ defmodule Quill.SessionController do
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp logout(conn) do
+    configure_session(conn, drop: true)
   end
 end
